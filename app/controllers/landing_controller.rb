d8cfe1abb68b66299b69b397fb00b6784abf04f8
kmazanec <k.mazanec@gmail.com>
2013-11-12 12:24:12 -0600

    add landing page for dev forward

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -8,10 +8,12 @@ class LandingController < ApplicationController
       render :adopt_a_coder
     when "sleepy-fjord-9426.herokuapp.com"
       render :adopt_a_coder
-    when "www.keithmazanec.com"
+    when "www.keithmazanec.com" || "keithmazanec.com"
       render :keithmazanec
+    when "www.devforward.org" || "www.devforward.com"
+      render :dev_forward
     when "localhost"
-      render :keithmazanec
+      render :dev_forward
     else
       render :error
     end
